b79de825a4b4ea1d35d3f0d3a22c2cba881b8dc9
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-12-04 15:43:02 +0100

    chore: add dist build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        dts({rollupTypes: false, tsconfigPath: './tsconfig.app.json'}),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            formats: ["es"],
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+        outDir: "dist",
+    },
 })
