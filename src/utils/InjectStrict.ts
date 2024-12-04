dec441e7f04a0d80ba95fb18f9f80ada1098e898
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-12-04 15:43:27 +0100

    bug: convert Symbol to string

--- src/utils/InjectStrict.ts
+++ src/utils/InjectStrict.ts
@@ -4,7 +4,7 @@ export const injectStrict = <T>(key: InjectionKey<T>) => {
   const injected = inject(key);
 
   if (!injected) {
-    throw "No injection for key " + key + "found";
+    throw "No injection for key " + key.toString() + "found";
   }
 
   return injected;
